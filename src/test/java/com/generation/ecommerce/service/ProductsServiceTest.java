39ad281164694a1d78a32b14d85f5d248e334e10
Josue Cano <jdevelopcano@gmail.com>
2025-04-11 14:55:17 -0600

    added testing with mockito

--- src/test/java/com/generation/ecommerce/service/ProductsServiceTest.java
+++ src/test/java/com/generation/ecommerce/service/ProductsServiceTest.java
@@ -11,9 +11,13 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.BDDMockito.given;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
 @ActiveProfiles("test")
@@ -44,6 +48,54 @@ public class ProductsServiceTest {
         assertThat(productAdded).isNotNull();
     }
 
+    @DisplayName("Test para probar que el metodo getProductById del servicio funciona")
+    @Test
+    public void onGetProductById(){
+        long productId = 1L;
+        given(productsRepository.findById(productId)).willReturn(Optional.of(product));
+        Products productSaved = productsService.getProductById(productId);
+        assertThat(productSaved).isNotNull();
+    }
+
+    @DisplayName("Test para probar que el metodo deleteProductById del servcio funciona")
+    @Test
+    public void onDeleteProductById(){
+        long productId = 1L;
+
+        when(productsRepository.existsById(productId)).thenReturn(true);
+        when(productsRepository.findById(productId)).thenReturn(Optional.of(product));
+
+        Products deletedProduct = productsService.deleteProductById(productId);
+
+        assertNotNull(deletedProduct);
+        assertEquals(productId,deletedProduct.getId());
+        verify(productsRepository).existsById(productId);
+        verify(productsRepository).findById(productId);
+        verify(productsRepository).deleteById(productId);
+    }
+
+    @DisplayName("Test para probar que el metodo updateProductById del servcio funcion")
+    @Test
+    public void onUpdateProductById(){
+        long productId = 1L;
+
+        Products productDetails =  new Products();
+        productDetails.setName("crema cerave");
+        productDetails.setDescription("Crema para el cuerpo cerave sin aroma");
+        productDetails.setQuantity(4);
+
+        when(productsRepository.findById(productId)).thenReturn(Optional.of(product));
+        when(productsRepository.save(any(Products.class))).thenAnswer(invocation -> invocation.getArgument(0));
+
+        Products updatedProduct = productsService.updateProduct(productId, productDetails);
+
+        assertEquals("crema cerave", updatedProduct.getName());
+        assertEquals("Crema para el cuerpo cerave sin aroma", updatedProduct.getDescription());
+        assertEquals(4, updatedProduct.getQuantity());
+
+        verify(productsRepository).findById(productId);
+        verify(productsRepository).save(product);
+    }
 
 
 
